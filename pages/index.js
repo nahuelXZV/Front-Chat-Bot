af6f16d9a7df1426dd1a3f95775dfdec142d0346
Nahuel Zalazar <zvnahuel63@gmail.com>
2022-10-25 19:20:38 -0400

    cambio de diseño de las cards

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
-
+import Card from "../components/Card";
 export default function Home() {
   return (
     <Layout title="Home">
       <div className={styles.container}>
         <h1 className={styles.title}>Home</h1>
+        <Card />
+
       </div>
     </Layout>
   );
