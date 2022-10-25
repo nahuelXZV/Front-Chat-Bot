54c1a103e6e1eb04483d61d05d23628a0c8a45b5
Nahuel Zalazar <zvnahuel63@gmail.com>
2022-10-25 14:01:18 -0400

    modal implementado

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,15 @@
 import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
+import Modal from "../components/ModalSimple";
+import ModalTail from "../components/ModalTail";
 
 export default function Login() {
   return (
     <Layout title="Login">
       <div className={styles.container}>
         <h1 className={styles.title}>Login</h1>
+        <Modal />
+        <ModalTail />
       </div>
     </Layout>
   );
--- pages/tablero.js
+++ pages/tablero.js
@@ -3,9 +3,90 @@ import Board from "react-trello";
 import React, { Component } from "react";
 
 export default function Tablero({ data }) {
+  const [showModal, setShowModal] = React.useState(false);
+  const [dataModal, setDataModal] = React.useState({});
+
   return (
     <Layout title="Tablero">
-      <Board data={data} draggable style={{ backgroundColor: "#1e3a8a" }} />
+      <Board
+        data={data}
+        draggable={false}
+        cardDraggable={true}
+        onCardClick={(cardId, metadata, laneId) => {
+          setDataModal({
+            ...metadata,
+            laneId,
+          });
+          setShowModal(true);
+        }}
+        style={{ backgroundColor: "#1e3a8a" }}
+      />
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  {/* imagen redonda pequena */}
+                  <img
+                    src={dataModal.prospecto?.foto}
+                    className="rounded-full w-10 h-10 mr-3"
+                  />
+                  <h3 className="text-3xl font-semibold mr-20">
+                    {dataModal.prospecto?.nombre} {dataModal.cliente?.nombre}
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      X
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    <span className="font-bold"> Correo: </span>{" "}
+                    {dataModal.cliente.correo} <br />
+                    <span className="font-bold">Telefono:</span>{" "}
+                    {dataModal.cliente.telefono} <br />
+                    <span className="font-bold">
+                      Frecuencia de compra:
+                    </span>{" "}
+                    {dataModal.frecuencia} <br />
+                    <span className="font-bold">Ult. Pedido: </span>
+                    {dataModal.ultimoPedido?.fecha}
+                    <br />
+                    <span className="font-bold">Promedio de compras:</span>{" "}
+                    {dataModal.promedioCompra}
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Cerrar
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Ver Pedidos
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
     </Layout>
   );
 }
@@ -57,7 +138,8 @@ function formatoDescripcion(data, type) {
         data.ultimoPedido?.fecha +
         "\n" +
         "Monto Ult. Pedido: " +
-        data.ultimoPedido?.montoTotal;
+        data.ultimoPedido?.montoTotal +
+        " Bs";
       return descripcionCliente;
       break;
     default:
@@ -68,14 +150,15 @@ function formatoDescripcion(data, type) {
         "Telefono: " +
         data.cliente.telefono +
         "\n" +
-        "Link Facebook: " +
-        data.cliente.facebookId +
+        "Frecuencia de pedidos: " +
+        data.frecuencia +
         "\n" +
         "Ult. Pedido: " +
         data.ultimoPedido?.fecha +
         "\n" +
-        "Monto Ult. Pedido: " +
-        data.ultimoPedido?.montoTotal;
+        "Promedio de compra: " +
+        data.promedioCompra +
+        " Bs";
       return clienteFrecuente;
       break;
   }
@@ -93,7 +176,6 @@ export async function getServerSideProps(context) {
   const prospectos = await fetch(`http://localhost:3010/api/prospectos/`);
   const datas = await prospectos.json();
   const body = datas.body;
-  console.log(body);
   body.forEach((element) => {
     if (element.contactos == 0) {
       let prospecto = {
@@ -101,6 +183,14 @@ export async function getServerSideProps(context) {
         title: element.prospecto.nombre,
         description: formatoDescripcion(element, "prospecto"),
         label: element.ultimoIngreso.fecha.slice(0, 10),
+        tags: [
+          {
+            bgcolor: "#0079BF",
+            color: "white",
+            title: "Ingresos: " + element.ingresos,
+          },
+        ],
+        metadata: element,
       };
       listProspectos.push(prospecto);
     } else {
@@ -109,6 +199,19 @@ export async function getServerSideProps(context) {
         title: element.prospecto.nombre,
         description: formatoDescripcion(element, "contacto"),
         label: element.ultimoIngreso.fecha.slice(0, 10),
+        tags: [
+          {
+            bgcolor: "#0079BF",
+            color: "white",
+            title: "Ingresos: " + element.ingresos,
+          },
+          {
+            bgcolor: "#7c3aed",
+            color: "white",
+            title: "Contactos: " + element.contactos,
+          },
+        ],
+        metadata: element,
       };
       listContactos.push(prospecto);
     }
@@ -140,6 +243,19 @@ export async function getServerSideProps(context) {
         title: element.cliente.nombre,
         description: formatoDescripcion(element, "cliente"),
         label: "Cliente",
+        tags: [
+          {
+            bgcolor: "#0079BF",
+            color: "white",
+            title: "Ingresos: " + element.Ingresos,
+          },
+          {
+            bgcolor: "#7c3aed",
+            color: "white",
+            title: "Pedidos: " + element.pedidos,
+          },
+        ],
+        metadata: element,
       };
       listClientes.push(cliente);
     } else {
@@ -148,6 +264,19 @@ export async function getServerSideProps(context) {
         title: element.cliente.nombre,
         description: formatoDescripcion(element, "frecuente"),
         label: "Frecuente",
+        tags: [
+          {
+            bgcolor: "#0079BF",
+            color: "white",
+            title: "Notificaciones: " + element.notificaciones,
+          },
+          {
+            bgcolor: "#7c3aed",
+            color: "white",
+            title: "Pedidos: " + element.pedidos,
+          },
+        ],
+        metadata: element,
       };
       listFrecuentes.push(cliente);
     }
